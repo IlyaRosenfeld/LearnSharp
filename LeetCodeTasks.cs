bfcb9cda9e96b530f2f0d19490814bba8205e923
Ilya <ilyarozenfeld70@gmail.com>
2024-10-11 22:51:13 +0300

    Add project files.

---
+++ LeetCodeTasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLearnSharp
+{
+    internal class LeetCodeTasks
+    {
+
+        static int LongestValidParentheses(string s)
+        {
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MLearnSharp
+{
+    class Program
+    {   
+        
+        static string DecToBool(int number)
+        {
+            int maxPow = 0;
+            while(number > Math.Pow(2, maxPow))
+            {
+                maxPow++;
+            }
+
+            Console.WriteLine(maxPow);
+
+            string result = "";
+
+            for(int i = maxPow; i >= 0; i--)
+            {
+                bool isBitSet = (number & (1 << i)) != 0;
+                if (isBitSet){
+                    result += '1';
+                }
+                else {
+                    result += '0';
+                }
+            }
+
+            return result;
+        }
+
+
+        
+        
+        
+
+
+
+        static void Main()
+           {
+
+            
+
+
+           }
+    }
+}
