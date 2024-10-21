a695e7ad0bf0442e9e3845df793527d66034db19
Ilya <ilyarozenfeld70@gmail.com>
2024-10-21 23:25:50 +0300

    First

--- LeetCodeTasks.cs
+++ LeetCodeTasks.cs
@@ -9,16 +9,52 @@ namespace MLearnSharp
     internal class LeetCodeTasks
     {
 
-        static int LongestValidParentheses(string s)
+        public bool IsValid(string s)
         {
-            int result = 0;
-
-            for (int i = 0; i < s.Length; i++)
+            var count = 0;
+            foreach (var c in s)
             {
+                if (c == '(')
+                    count++;
 
+                if (c == ')')
+                {
+                    if (count == 0) return false;
+                    count--;
+                }
+            }
+            return count == 0;
+        }
+
+        
+            public int LongestValidParentheses(string s)
+            {
+                int count = 0;
+                int maxCount = 0;
+                int[] sums = new int[s.Length];
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '(')
+                        count++;
+                    if (s[i] == ')')
+                    {
+                        if (count > 0)
+                        {
+                            count--;
+                            sums[i] = sums[i - 1] + 2;
+                            if (sums[i] < i)
+                            {
+                                sums[i] += sums[i - sums[i]];
+                            }
+                        }
+                    }
+                    maxCount = Math.Max(maxCount, sums[i]);
+                }
+
+                return maxCount;
             }
 
-            return result;
+            
         }
     }
-}
+
